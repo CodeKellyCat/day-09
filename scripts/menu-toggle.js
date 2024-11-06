d10269e8eee1454f7211a2d5b32b7951bea51f15
Kelly McLaughlin <kmclaughlin9@my.bcit.ca>
2024-11-06 11:15:24 -0800

    added menu toggle option 1 and 2

--- scripts/menu-toggle.js
+++ scripts/menu-toggle.js
@@ -1,4 +1,16 @@
 // Main Navigation Toggle
-
-
+const button = document.getElementById("main-nav-button")
+const links = document.getElementById("main-nav-list")
 // Thumb Navigation Toggle
+button.addEventListener('click', funtion() {
+    links.classList.toggle('toggled');
+
+})
+
+// Main Navigation Toggle
+// const button = document.getElementById("main-nav-button");
+// const links = document.getElementById("main-nav-list");
+
+// button.addEventListener('click', function() {
+//     links.classList.toggle('toggled');
+// })
